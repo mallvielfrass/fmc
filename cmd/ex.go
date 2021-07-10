7b7612b235f6922489ab38d12d4bf64f1ca3b22e
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-07-10 14:25:53 +0300

    add example

--- cmd/ex.go
+++ cmd/ex.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"errors"
 
 	"github.com/mallvielfrass/fmc"
 )
@@ -28,46 +28,55 @@ type Config struct {
 	} `json:"database"`
 }
 
+func beta() {
+	fmc.ErrorHandle(errors.New("Err"), "file")
+}
+func betaR() {
+	fmc.ErrorHandleFatal(errors.New("Err"), "file")
+}
 func main() {
-	fmc.Printf("#yst I#ybt  love#bbt  Go#gbt!\n")
-	fmc.Printf("#ybt I#rbt  love#bbt  Go#gbt!\n")
-	fmc.Printf("#ybt %s#rbt  %s#bbt  %s#gbt%s\n", "I", "love", "Go", "!")
-	fmc.Println("#ybt lol")
-	fmc.Printfln("#ybt %s", "I")
-	fmc.Println("#bbt Println")
-	fmc.Printfln(`
-	#RRR1) #bst  !#bst aqua 		#bbt !#bbt bold aqua
-	#RRR2) #0st  !#0st black 		#0bt !#0bt bold black	
-	#RRR3) #Bst  !#Bst blue  		#Bbt !#Bbt bold blue	
-	#RRR4) #fst  !#fst fuchsia     	#fbt !#fbt bold fuchsia	
-	#RRR5) #1st  !#1st gray 		#1bt !#1bt bold gray	
-	#RRR6) #Gst  !#Gst green 		#Gbt !#Gbt bold green	
-	#RRR7) #gst  !#gst lime 		#gbt !#gbt bold lime	
-	#RRR8) #mst  !#mst maroon		#mbt !#mbt bold maroon	
-	#RRR9) #nst  !#nst navy 		#nbt !#nbt bold navy	
-	#RRR10) #ost !#ost olive		#obt !#obt bold olive	
-	#RRR11) #pst !#pst purple		#pbt !#pbt bold purple	
-	#RRR12) #rst !#rst red   		#rbt !#rbt bold red
-	#RRR13) #sst !#sst silver		#sbt !#sbt bold silver
-	#RRR14) #tst !#tst teal        	#tbt !#tbt bold teal
-	#RRR15) #wst !#wst white		#wbt !#wbt bold white
-	#RRR16) #yst !#yst yellow		#ybt !#ybt bold yellow
-	#RRR17) #RRR !#RRR reset to default terminal font color 
-	`)
-	jsonConfig := []byte(`{
-        "server":{
-            "host":"localhost",
-            "port":"8080"},
-        "database":{
-            "host":"localhost",
-            "user":"db_user",
-            "password":"supersecret",
-            "db":"my_db"}}`)
-	var config Config
-	err := json.Unmarshal(jsonConfig, &config)
-	if err != nil {
-		panic(err)
-	}
-	fmc.PrintStruct(config)
-
+	// fmc.Printf("#yst I#ybt  love#bbt  Go#gbt!\n")
+	// fmc.Printf("#ybt I#rbt  love#bbt  Go#gbt!\n")
+	// fmc.Printf("#ybt %s#rbt  %s#bbt  %s#gbt%s\n", "I", "love", "Go", "!")
+	// fmc.Println("#ybt lol")
+	// fmc.Printfln("#ybt %s", "I")
+	// fmc.Println("#bbt Println")
+	// fmc.Printfln(`
+	// #RRR1) #bst  !#bst aqua 		#bbt !#bbt bold aqua
+	// #RRR2) #0st  !#0st black 		#0bt !#0bt bold black
+	// #RRR3) #Bst  !#Bst blue  		#Bbt !#Bbt bold blue
+	// #RRR4) #fst  !#fst fuchsia     	#fbt !#fbt bold fuchsia
+	// #RRR5) #1st  !#1st gray 		#1bt !#1bt bold gray
+	// #RRR6) #Gst  !#Gst green 		#Gbt !#Gbt bold green
+	// #RRR7) #gst  !#gst lime 		#gbt !#gbt bold lime
+	// #RRR8) #mst  !#mst maroon		#mbt !#mbt bold maroon
+	// #RRR9) #nst  !#nst navy 		#nbt !#nbt bold navy
+	// #RRR10) #ost !#ost olive		#obt !#obt bold olive
+	// #RRR11) #pst !#pst purple		#pbt !#pbt bold purple
+	// #RRR12) #rst !#rst red   		#rbt !#rbt bold red
+	// #RRR13) #sst !#sst silver		#sbt !#sbt bold silver
+	// #RRR14) #tst !#tst teal        	#tbt !#tbt bold teal
+	// #RRR15) #wst !#wst white		#wbt !#wbt bold white
+	// #RRR16) #yst !#yst yellow		#ybt !#ybt bold yellow
+	// #RRR17) #RRR !#RRR reset to default terminal font color
+	// `)
+	// jsonConfig := []byte(`{
+	//     "server":{
+	//         "host":"localhost",
+	//         "port":"8080"},
+	//     "database":{
+	//         "host":"localhost",
+	//         "user":"db_user",
+	//         "password":"supersecret",
+	//         "db":"my_db"}}`)
+	// var config Config
+	// err := json.Unmarshal(jsonConfig, &config)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmc.PrintStruct(config)
+	beta()
+	fmc.ErrorHandle(errors.New("Err"), "file")
+	betaR()
+	//fmc.ErrorHandleFatal(errors.New("Err"))
 }
