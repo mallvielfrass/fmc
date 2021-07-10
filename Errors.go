5e313fd071de5ec9a86c02b7f587b8a3045084ce
mallvielfrass <masterharoldsaxon4@gmail.com>
2021-07-10 14:28:10 +0300

    add Hadle for Error and FatalHandler(as log.fatal)

---
+++ Errors.go
@@ -0,0 +1,29 @@
+package fmc
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var std = log.New(os.Stderr, "", log.LstdFlags)
+
+func errorHandlePrepare(e error, a ...interface{}) string {
+	//if e != nil {
+	path, line, fn := WhoCallerErr()
+	if a != nil {
+
+		return fmt.Sprintf(Sprint("#rbtError #RRR[#gbt%s#RRR:#gbt%s #ybt%s()#RRR]: \n\tDescription: #RRR[#ybt %s#RRR] \n\t#RRRMessage: #ybt%s"), path, line, fn, fmt.Sprintf("%v", a[0]), e.Error())
+
+	}
+	return fmt.Sprintf(Sprint("#rbtError#RRR[#gbt%s#RRR:#gbt%s #ybt%s()#RRR]: \n\t#RRRMessage: #ybt%s"), e.Error())
+	//}
+	//return ""
+}
+func ErrorHandle(e error, a ...interface{}) {
+	fmt.Println(errorHandlePrepare(e, a[0]))
+}
+func ErrorHandleFatal(e error, a ...interface{}) {
+	std.Output(2, errorHandlePrepare(e, a[0]))
+	os.Exit(1)
+}
